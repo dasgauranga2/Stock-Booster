e58d0d62702a0026c3c83e444342d387f2572881
Gauranga Das <gaurangadas@Gaurangas-MacBook-Air.local>
2022-07-20 13:21:42 +0530

    Plot Stock Price History line chart

--- home.js
+++ home.js
@@ -3,6 +3,8 @@ const search_button = document.querySelector("#input-container input[type='submi
 const search_field = document.querySelector("#input-container input[type='text']");
 let API_KEY;
 
+// CHARTIST.JS - http://gionkunz.github.io/chartist-js/examples.html#simple-line-chart
+
 // get the alpha vantage api key
 fetch('API_KEY.txt')
     .then(function(response) {
@@ -22,6 +24,8 @@ search_button.addEventListener('click',
         const symbol = search_field.value;
         // get the current stock price
         stock_price(symbol);
+        // get stock price history
+        stock_price_history(symbol);
 });
 
 // function to get current stock price of a company
@@ -65,4 +69,69 @@ function stock_price(company) {
         prices[2].innerHTML = `Low : ${Number(low).toFixed(2)}`;
         
     });
+}
+
+// function to get company stock price history
+function stock_price_history(company) {
+    // web api url
+    const URL = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${company}&outputsize=full&apikey=${API_KEY}`;
+    // use the fetch api to get the data
+    fetch(URL)
+    .then(function(response) {
+        // the response received has a json method which is a promise 
+        // return the promise
+        return response.json();
+    })
+    .then(function(data) {
+        // get the data from web api
+        console.log(data);
+        // stock price history
+        const history = data['Time Series (Daily)'];
+        console.log(history);
+        // get date strings
+        const dates = date_strings(190);
+        // array of stock prices
+        const prices = [];
+        for (let date of dates) {
+            if (date in history) {
+                // get the stock price
+                const price = history[date]['4. close'];
+                prices.push(price);
+            }
+        }
+        console.log(prices);
+        // plot the line chart
+        new Chartist.Line('.ct-chart', {
+            // labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+            series: [
+                prices
+            ]
+          }, {
+            fullWidth: true,
+            showPoint: false,
+            chartPadding: {
+              right: 40
+            }
+          });
+    });
+}
+
+// function to generate date strings
+function date_strings(days) {
+    // current timestamp
+    let timestamp = Date.now();
+    // milliseconds in a day
+    const ms_day = 86400000;
+    // array of date strings
+    const dates = [];
+    while (days > 0) {
+        // get the date from timestamp
+        const date = new Date(timestamp).toISOString();
+        // get the date string
+        const date_string = date.split('T')[0];
+        dates.push(date_string);
+        timestamp = timestamp-ms_day;
+        days--;
+    }
+    return dates.reverse();
 }
