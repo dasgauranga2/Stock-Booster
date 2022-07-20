4889d5589bd3b0a5d69f7ad787c287a50dc163ba
Gauranga Das <gaurangadas@Gaurangas-MacBook-Air.local>
2022-07-20 09:07:19 +0530

    Display company current stock prices

---
+++ home.js
@@ -0,0 +1,55 @@
+
+const search_button = document.querySelector("#input-container input[type='submit']");
+const search_field = document.querySelector("#input-container input[type='text']");
+let API_KEY;
+
+// get the alpha vantage api key
+fetch('API_KEY.txt')
+    .then(function(response) {
+        // the response received has a text method which is a promise 
+        // return the promise
+        return response.text();
+    })
+    .then(function(data) {
+        // get the data from the text file
+        API_KEY = data;
+    });
+
+// detect search button click
+search_button.addEventListener('click',
+    function search_stocks() {
+        // get the company symbol 
+        const symbol = search_field.value;
+        // get the current stock price
+        stock_price(symbol);
+});
+
+// function to get current stock price of a company
+function stock_price(company) {
+    // web api url
+    const URL = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${company}&apikey=${API_KEY}`;
+    // use the fetch api to get the data
+    fetch(URL)
+    .then(function(response) {
+        // the response received has a json method which is a promise 
+        // return the promise
+        return response.json();
+    })
+    .then(function(data) {
+        // get the data from web api
+        const stock_price = data['Global Quote'];
+        console.log(stock_price);
+        // get the stock prices
+        const open = stock_price['02. open'];
+        const high = stock_price['03. high'];
+        const low = stock_price['04. low'];
+        const price = stock_price['05. price'];
+        // select the html elements
+        const prices = document.querySelectorAll('#stock-price p');
+        // set the prices
+        prices[0].innerHTML = `Open : ${Number(open).toFixed(2)}`;
+        prices[1].innerHTML = `Current : ${Number(price).toFixed(2)}`;
+        prices[2].innerHTML = `High : ${Number(high).toFixed(2)}`;
+        prices[3].innerHTML = `Open : ${Number(low).toFixed(2)}`;
+    });
+}
